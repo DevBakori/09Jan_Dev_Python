65ace8f58435f7bad689af04e7a847f556df6412
Devbakori <devbakori2141@gmail.com>
2024-01-16 10:54:56 +0530

    Lst updt

--- Module_1/test.py
+++ Module_1/test.py
@@ -1,1 +1,78 @@
-print("hello")
+# types of var :-   # int 
+                    # float
+                    # string
+                    # boolean 
+                    # complex
+
+
+# a=345
+# print(a)                              <<< cast
+# print(float(a))
+
+# a="22"
+# b="44"                                <<< cast 
+# print(a+b)
+# print (int(a)+int(b))
+
+
+
+# print ("Hello",end=" ")           <<< end 
+# print("Dev")
+
+
+# print("hello \npython")
+# print("he\bllo\bpython")             <<< escape
+# print(r"hello\tpython")
+
+
+# Mobile_No=input("Enter Your Number:")        <<< input
+# print("My Number Is",Mobile_No)
+
+
+# a=input("Enter value of A:")
+# b=input ("Enter value of B:")                <<< input
+# print("sum:",int(a)+int(b))
+
+
+# a=int(input ("Enter value of A:"))
+# b=int(input ("Enter value of B:"))           <<< input
+# print("sum:",(a%b))
+
+
+# print(hello python!
+# goodbye python!           <<< print
+# the last line)
+
+
+# print("hiii","hello", "my name is dev",sep="``")      <<<  seprate 
+
+
+# a=22
+# b=44
+# print("A",a)                  <<< print 2 var.
+# print ("B",b)
+
+
+a=22
+b=44
+tamp=a 
+#  <<<<  using 3rd var. swaping
+a=b
+b=tamp
+print("A =",a)
+print("B =",b)
+
+
+# a=22
+# b=44
+# a,b=b,            <<<<  using just 2 var. swaping
+# print('A =',a)
+# print("B =",b)
+
+
+# a=22                       
+# b=44
+# print("Add:",a+b)  
+# print("Sub:",a-b)                 <<<  vars
+# print("Mul:",a*b)
+# print("Div:",a/b)

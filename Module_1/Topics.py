c06b8cd4948b6418ff0ce29fd45cb9d63c5494b0
Devbakori <devbakori2141@gmail.com>
2024-01-18 11:39:29 +0530

    Condition Example

--- Module_1/Topics.py
+++ Module_1/Topics.py
@@ -76,3 +76,64 @@
 # print("Sub:",a-b)                 <<<  vars
 # print("Mul:",a*b)
 # print("Div:",a/b)
+
+
+
+
+
+# <---------------------------------------------Formate Function------------------------------------------------>
+# id=78
+# name="Sanket"
+#print("My ID is {} and Name is {}".format(id,name))
+
+# print(f"My ID is {id} and Name is {name}")
+
+
+# a=int(input("Enter value of A:"))
+# b=int(input("Enter value of B:"))
+# print(f"Valuue of A={a}, Value of B={b} and Sum of {a}+{b}={a+b}")
+
+
+
+
+
+# <-----------------------------------------if---------------------------------------------->
+# a=int(input("Enter value of A:"))
+# b=int(input("Enter value of B:"))
+
+# if a<b: #TRUE
+#     print("A is min")
+    
+    
+    
+# <--------------------------------------------if_elif------------------------------------------->    
+# a=int(input("Enter value of A:"))
+# b=int(input("Enter value of B:"))
+
+# if a<b:
+#     print("A is min")
+# elif a>b:
+#     print("A is max")
+# #elif a==b:
+# else:
+#     print("Both are equal...")
+
+
+
+
+# <-----------------------------------------if_else-------------------------------------------->
+# a=int(input("Enter value of A:"))
+# b=int(input("Enter value of B:"))
+
+# if a<b: #TRUE
+#     print("A is min")
+# else: #FALSE
+#     print("A is max")
+
+
+
+# <--------------------------------------------Split Function-------------------------------------------->
+mystr="This is Python!"
+print(mystr)
+print(mystr.split())
+print(mystr.split('i'))

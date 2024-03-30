83fe1a60eec72d0e4086510eaeaed1309a0136fe
DevBakori <109731775+DevBakori@users.noreply.github.com>
2024-03-30 08:26:18 +0530

    Delete Module_1 directory

--- Module_1/task_1.py
+++
@@ -1,8 +0,0 @@
-id=input("Enter Your Id :")
-Name=input("Enter Your Name :")
-Sub=input("Enter Your Subject :")
-
-print("Your Id is :" ,id)
-print("Your Id is :" ,Name)
-print("Your Id is :" ,Sub)
-
